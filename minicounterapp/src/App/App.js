639d8703916bbcfe82ca3a4aebe2c9676bf4a56f
pawelkellner <smrpawel@gmail.com>
2022-09-27 22:30:32 +0200

    add counter app

---
+++ minicounterapp/src/App/App.js
@@ -0,0 +1,12 @@
+import CounterCard from "../components/CounterCard/CounterCard";
+import "./App.css"
+const App = () => {
+    return(
+        <>
+        <CounterCard number="0"/>
+        <CounterCard number="0"/>
+        </>
+    );
+}
+
+export default App;
